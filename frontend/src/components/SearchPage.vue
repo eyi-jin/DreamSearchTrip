f5a5ffe608c2fe6839e935281523f70c407f4c53
정명진 <jmj2588547@naver.com>
2023-05-19 10:35:41 +0900

    update: 검색창 페이지 세부 수정

--- frontend/src/components/SearchPage.vue
+++ frontend/src/components/SearchPage.vue
@@ -1,59 +1,42 @@
 <template>
-  <div>
-    <div class="hero overlay">
-      <div class="img-bg rellax">
-        <img src="images/hero_2.jpg" alt="Image" class="img-fluid" />
-      </div>
-      <div class="container">
-        <div class="row align-items-center justify-content-start">
-          <div class="col-lg-6 mx-auto text-center">
-            <h1 class="heading" data-aos="fade-up">Search</h1>
-            <p class="mb-4" data-aos="fade-up">
-              A small river named Duden flows by their place and supplies it with the necessary
-              regelialia. It is a paradisematic country, in which roasted parts of sentences fly
-              into your mouth.
-            </p>
-
-            <div data-aos="fade-up">
-              <a
-                href="https://www.youtube.com/watch?v=5n-e6lOhVq0"
-                class="play-button glightbox3 w-100 text-center"
-              >
-                <div class="d-inline-flex align-items-center">
-                  <span class="icon-button me-3">
-                    <span class="icon-play"></span>
-                  </span>
-                  <span class="caption">Watch Video</span>
-                </div>
-              </a>
+    <div>
+        <div class="hero overlay">
+            <div class="img-bg rellax">
+                <img src="images/hero_2.jpg" alt="Image" class="img-fluid" />
             </div>
-          </div>
+            <div class="container">
+                <div class="row align-items-center justify-content-start">
+                    <div class="col-lg-6 mx-auto text-center">
+                        <h1 class="heading" data-aos="fade-up">Search</h1>
+                        <p class="mb-4" data-aos="fade-up">
+                            {{ userName }}님 여행지를 검색해보세요
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-    <div class="container">
-      <search-trip-form></search-trip-form>
-      <kakao-map></kakao-map>
-      <card-view></card-view>
+        <div class="mx-3">
+            <search-trip-form></search-trip-form>
+            <kakao-map></kakao-map>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import CardView from "./ui/CardView.vue";
 import KakaoMap from "./ui/KakaoMap.vue";
 import SearchTripForm from "./ui/SearchTripForm.vue";
 
 export default {
-  components: { CardView, KakaoMap, SearchTripForm },
-  data() {
-    return {
-      message: "",
-    };
-  },
-  created() {},
-  methods: {},
+    components: { KakaoMap, SearchTripForm },
+    data() {
+        return {
+            message: "",
+            userName: sessionStorage.getItem("userName"),
+        };
+    },
+    created() {},
+    methods: {},
 };
 </script>
 
--- frontend/src/components/ui/SearchTripForm.vue
+++ frontend/src/components/ui/SearchTripForm.vue
@@ -1,215 +1,207 @@
 <template>
-  <div class="d-flex m-3 justify-content-center">
-    <form class="d-flex my-3">
-      <select id="area1List" class="form-select me-3">
-        <option value="">시도를 선택하세요</option>
-      </select>
-      <select id="area2List" class="form-select me-3">
-        <option value="">구군을 선택하세요</option>
-      </select>
+    <div class="d-flex row">
+        <form class="col-md-11 d-flex my-3">
+            <select id="area1List" class="form-select me-3">
+                <option value="">시도를 선택하세요</option>
+            </select>
+            <select id="area2List" class="form-select me-3">
+                <option value="">구군을 선택하세요</option>
+            </select>
 
-      <select id="cat1List" class="form-select me-3">
-        <option value="">대분류 선택하세요</option>
-      </select>
-      <select id="cat2List" class="form-select me-3">
-        <option value="">중분류 선택하세요</option>
-      </select>
-      <select id="cat3List" class="form-select me-3">
-        <option value="">소분류 선택하세요</option>
-      </select>
-    </form>
-    <button id="btnSearch" type="button" class="btn btn-primary mt-3 mb-3">
-      검색
-    </button>
-  </div>
+            <select id="cat1List" class="form-select me-3">
+                <option value="">대분류를 선택하세요</option>
+            </select>
+            <select id="cat2List" class="form-select me-3">
+                <option value="">중분류를 선택하세요</option>
+            </select>
+            <select id="cat3List" class="form-select me-3">
+                <option value="">소분류를 선택하세요</option>
+            </select>
+        </form>
+        <button id="btnSearch" type="button" class="col-md-1 btn btn-primary my-3">검색</button>
+    </div>
 </template>
 
 <script>
 import http from "@/components/common/axios";
 
 export default {
-  data() {
-    return {
-      numOfRows: 10,
-      pageNo: 1,
-      areaCode: "",
-      sigunguCode: "",
-      cat1: "",
-      cat2: "",
-      cat3: "",
-    };
-  },
-  async mounted() {
-    await this.getArea1List();
-    await this.getCat1List();
-
-    let $this = this;
-    document.querySelector("#btnSearch").onclick = function () {
-      $this.sigunguCode = document.querySelector("#area2List").value;
-      $this.cat3 = document.querySelector("#cat3List").value;
-      $this.getList();
-    };
-  },
-  methods: {
-    getList: async function () {
-      let url = `/trip/list`;
-      let urlParams =
-        "?numOfRows=" +
-        this.numOfRows +
-        "&pageNo=" +
-        this.pageNo +
-        "&areaCode=" +
-        this.areaCode +
-        "&sigunguCode=" +
-        this.sigunguCode +
-        "&cat1=" +
-        this.cat1 +
-        "&cat2=" +
-        this.cat2 +
-        "&cat3=" +
-        this.cat3;
-
-      let response = await http.get(url + urlParams);
-      let { data } = response;
-      // this.makeCard(data);
-      console.log(data);
-
-      let itemList = data.response.body.items.item;
-      console.log(itemList);
+    data() {
+        return {
+            numOfRows: 10,
+            pageNo: 1,
+            areaCode: "",
+            sigunguCode: "",
+            cat1: "",
+            cat2: "",
+            cat3: "",
+        };
     },
+    async mounted() {
+        await this.getArea1List();
+        await this.getCat1List();
 
-    getArea1List: async function () {
-      let $this = this;
-      let url = `trip/areaCode`;
-      let urlParams =
-        "?numOfRows=" + this.numOfRows + "&pageNo= " + this.pageNo;
-
-      let response = await http.get(url + urlParams);
-      let { data } = response;
-      console.log(data);
-
-      let codeList = data.response.body.items.item;
-      console.log(codeList);
-
-      let listHtml = `<option value=''>시도를 선택하세요</option>`;
-      codeList.forEach((el) => {
-        listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
-      });
-      document.querySelector("#area1List").innerHTML = listHtml;
-
-      // change event handler
-      document.querySelector("#area1List").onchange = function (e) {
-        $this.areaCode = e.target.value;
-        $this.getArea2List();
-      };
+        let $this = this;
+        document.querySelector("#btnSearch").onclick = function () {
+            $this.sigunguCode = document.querySelector("#area2List").value;
+            $this.cat3 = document.querySelector("#cat3List").value;
+            $this.getList();
+        };
     },
+    methods: {
+        getList: async function () {
+            let url = `/trip/list`;
+            let urlParams =
+                "?numOfRows=" +
+                this.numOfRows +
+                "&pageNo=" +
+                this.pageNo +
+                "&areaCode=" +
+                this.areaCode +
+                "&sigunguCode=" +
+                this.sigunguCode +
+                "&cat1=" +
+                this.cat1 +
+                "&cat2=" +
+                this.cat2 +
+                "&cat3=" +
+                this.cat3;
 
-    getArea2List: async function () {
-      let url = `trip/areaCode`;
-      let urlParams =
-        "?numOfRows=" +
-        this.numOfRows +
-        "&pageNo=" +
-        this.pageNo +
-        "&areaCode=" +
-        this.areaCode;
+            let response = await http.get(url + urlParams);
+            let { data } = response;
+            // this.makeCard(data);
+            console.log(data);
 
-      let response = await http.get(url + urlParams);
-      let { data } = response;
-      console.log(data);
+            let itemList = data.response.body.items.item;
+            console.log(itemList);
+        },
 
-      let codeList = data.response.body.items.item;
-      console.log(codeList);
+        getArea1List: async function () {
+            let $this = this;
+            let url = `trip/areaCode`;
+            let urlParams = "?numOfRows=" + this.numOfRows + "&pageNo= " + this.pageNo;
 
-      let listHtml = `<option value=''>구군을 선택하세요</option>`;
-      codeList.forEach((el) => {
-        listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
-      });
-      document.querySelector("#area2List").innerHTML = listHtml;
+            let response = await http.get(url + urlParams);
+            let { data } = response;
+            console.log(data);
+
+            let codeList = data.response.body.items.item;
+            console.log(codeList);
+
+            let listHtml = `<option value=''>시도를 선택하세요</option>`;
+            codeList.forEach((el) => {
+                listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
+            });
+            document.querySelector("#area1List").innerHTML = listHtml;
+
+            // change event handler
+            document.querySelector("#area1List").onchange = function (e) {
+                $this.areaCode = e.target.value;
+                $this.getArea2List();
+            };
+        },
+
+        getArea2List: async function () {
+            let url = `trip/areaCode`;
+            let urlParams =
+                "?numOfRows=" +
+                this.numOfRows +
+                "&pageNo=" +
+                this.pageNo +
+                "&areaCode=" +
+                this.areaCode;
+
+            let response = await http.get(url + urlParams);
+            let { data } = response;
+            console.log(data);
+
+            let codeList = data.response.body.items.item;
+            console.log(codeList);
+
+            let listHtml = `<option value=''>구군을 선택하세요</option>`;
+            codeList.forEach((el) => {
+                listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
+            });
+            document.querySelector("#area2List").innerHTML = listHtml;
+        },
+
+        getCat1List: async function () {
+            let $this = this;
+            let url = `trip/categoryCode`;
+            let urlParams = "?numOfRows=" + this.numOfRows + "&pageNo=" + this.pageNo;
+
+            let response = await http.get(url + urlParams);
+            let { data } = response;
+            console.log(data);
+
+            let codeList = data.response.body.items.item;
+            console.log(codeList);
+
+            let listHtml = `<option value=''>대분류 선택하세요</option>`;
+            codeList.forEach((el) => {
+                listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
+            });
+            document.querySelector("#cat1List").innerHTML = listHtml;
+
+            // change event handler
+            document.querySelector("#cat1List").onchange = function (e) {
+                $this.cat1 = e.target.value;
+                $this.getCat2List();
+            };
+        },
+
+        getCat2List: async function () {
+            let $this = this;
+            let url = `trip/categoryCode`;
+            let urlParams =
+                "?numOfRows=" + this.numOfRows + "&pageNo=" + this.pageNo + "&cat1=" + this.cat1;
+
+            let response = await http.get(url + urlParams);
+            let { data } = response;
+            console.log(data);
+
+            let codeList = data.response.body.items.item;
+            console.log(codeList);
+
+            let listHtml = `<option value=''>중분류 선택하세요</option>`;
+            codeList.forEach((el) => {
+                listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
+            });
+            document.querySelector("#cat2List").innerHTML = listHtml;
+
+            // change event handler
+            document.querySelector("#cat2List").onchange = function (e) {
+                $this.cat2 = e.target.value;
+                $this.getCat3List();
+            };
+        },
+
+        getCat3List: async function () {
+            let url = `trip/categoryCode`;
+            let urlParams =
+                "?numOfRows=" +
+                this.numOfRows +
+                "&pageNo=" +
+                this.pageNo +
+                "&cat1=" +
+                this.cat1 +
+                "&cat2=" +
+                this.cat2;
+
+            let response = await http.get(url + urlParams);
+            let { data } = response;
+            console.log(data);
+
+            let codeList = data.response.body.items.item;
+            console.log(codeList);
+
+            let listHtml = `<option value=''>소분류 선택하세요</option>`;
+            codeList.forEach((el) => {
+                listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
+            });
+            document.querySelector("#cat3List").innerHTML = listHtml;
+        },
     },
-
-    getCat1List: async function () {
-      let $this = this;
-      let url = `trip/categoryCode`;
-      let urlParams = "?numOfRows=" + this.numOfRows + "&pageNo=" + this.pageNo;
-
-      let response = await http.get(url + urlParams);
-      let { data } = response;
-      console.log(data);
-
-      let codeList = data.response.body.items.item;
-      console.log(codeList);
-
-      let listHtml = `<option value=''>대분류 선택하세요</option>`;
-      codeList.forEach((el) => {
-        listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
-      });
-      document.querySelector("#cat1List").innerHTML = listHtml;
-
-      // change event handler
-      document.querySelector("#cat1List").onchange = function (e) {
-        $this.cat1 = e.target.value;
-        $this.getCat2List();
-      };
-    },
-
-    getCat2List: async function () {
-      let $this = this;
-      let url = `trip/categoryCode`;
-      let urlParams =
-        "?numOfRows=" +
-        this.numOfRows +
-        "&pageNo=" +
-        this.pageNo +
-        "&cat1=" +
-        this.cat1;
-
-      let response = await http.get(url + urlParams);
-      let { data } = response;
-      console.log(data);
-
-      let codeList = data.response.body.items.item;
-      console.log(codeList);
-
-      let listHtml = `<option value=''>중분류 선택하세요</option>`;
-      codeList.forEach((el) => {
-        listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
-      });
-      document.querySelector("#cat2List").innerHTML = listHtml;
-
-      // change event handler
-      document.querySelector("#cat2List").onchange = function (e) {
-        $this.cat2 = e.target.value;
-        $this.getCat3List();
-      };
-    },
-
-    getCat3List: async function () {
-      let url = `trip/categoryCode`;
-      let urlParams =
-        "?numOfRows=" +
-        this.numOfRows +
-        "&pageNo=" +
-        this.pageNo +
-        "&cat1=" +
-        this.cat1 +
-        "&cat2=" +
-        this.cat2;
-
-      let response = await http.get(url + urlParams);
-      let { data } = response;
-      console.log(data);
-
-      let codeList = data.response.body.items.item;
-      console.log(codeList);
-
-      let listHtml = `<option value=''>소분류 선택하세요</option>`;
-      codeList.forEach((el) => {
-        listHtml += "<option value=" + el.code + ">" + el.name + "</option>";
-      });
-      document.querySelector("#cat3List").innerHTML = listHtml;
-    },
-  },
 };
 </script>
 

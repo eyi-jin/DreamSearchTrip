1c0de197ca4b69feb62a2cc4f10bdf98794a1e7c
choi-jinseok <cj5255@naver.com>
2023-05-22 11:40:07 +0900

    add: signupPage

--- frontend/src/components/SearchPage.vue
+++ frontend/src/components/SearchPage.vue
@@ -1,40 +1,42 @@
 <template>
-    <div>
-        <div class="hero overlay">
-            <div class="img-bg rellax">
-                <img src="images/hero_2.jpg" alt="Image" class="img-fluid" />
-            </div>
-            <div class="container">
-                <div class="row align-items-center justify-content-start">
-                    <div class="col-lg-6 mx-auto text-center">
-                        <h1 class="heading" data-aos="fade-up">Search</h1>
-                        <p class="mb-4" data-aos="fade-up">
-                            {{ userName }}님 여행지를 검색해보세요
-                        </p>
-                    </div>
-                </div>
-            </div>
+  <div>
+    <div class="hero overlay">
+      <div class="img-bg rellax">
+        <img src="images/hero_2.jpg" alt="Image" class="img-fluid" />
+      </div>
+      <div class="container">
+        <div class="row align-items-center justify-content-start">
+          <div class="col-lg-6 mx-auto text-center">
+            <h1 class="heading" data-aos="fade-up">Search</h1>
+            <p class="mb-4" data-aos="fade-up">{{ userName }}님 여행지를 검색해보세요</p>
+          </div>
         </div>
-
-        <div class="mx-3">
-            <search-trip-form></search-trip-form>
-        </div>
+      </div>
     </div>
+
+    <div class="mx-3">
+      <search-trip-form></search-trip-form>
+    </div>
+  </div>
 </template>
 
 <script>
 import SearchTripForm from "./ui/SearchTripForm.vue";
 
 export default {
-    components: { SearchTripForm },
-    data() {
-        return {
-            message: "",
-            userName: sessionStorage.getItem("userName"),
-        };
-    },
-    created() {},
-    methods: {},
+  components: { SearchTripForm },
+  data() {
+    return {
+      message: "",
+      userName: sessionStorage.getItem("userName"),
+    };
+  },
+  created() {
+    if (!sessionStorage.getItem("userName")) {
+      this.$router.push("/login");
+    }
+  },
+  methods: {},
 };
 </script>
 

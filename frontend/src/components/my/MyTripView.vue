66af9461c11c051e3641e700b0a70617a5580f20
eyi-jin <jmj2588547@naver.com>
2023-05-23 21:08:48 +0900

    add: mytrip 보여주기

--- frontend/src/components/my/MyTripView.vue
+++ frontend/src/components/my/MyTripView.vue
@@ -1,9 +1,64 @@
 <template>
-  <div><h1>my trip 입니다!!</h1></div>
+  <div class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-12" data-aos="fade-up" data-aos-delay="0">
+          <h2 class="heading mb-5">My Trips</h2>
+        </div>
+      </div>
+      <div class="row align-items-stretch">
+        <div
+          v-for="(fav, index) in list"
+          :key="index"
+          class="col-6 col-sm-6 col-md-6 col-lg-3"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
+          <div class="media-entry">
+            <a href="#">
+              <img
+                :src="fav.favImgUrl"
+                alt="Image"
+                class="img-fluid"
+                style="height: 200px"
+              />
+            </a>
+            <div class="bg-white m-body">
+              <span class="date">May 14, 2020</span>
+              <h3>
+                <a href="#">{{ fav.favName }}</a>
+              </h3>
+              <p><b>주소:</b> {{ fav.favAddr }}</p>
+              <p><b>Tel:</b> {{ fav.favTel }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import http from "@/components/common/axios";
+
+export default {
+  data() {
+    return { list: [] };
+  },
+  mounted() {
+    this.getFavList();
+  },
+  methods: {
+    async getFavList() {
+      let url = `/fav/favView`;
+
+      let response = await http.get(url);
+      let { data } = response;
+      console.log(data);
+      this.list = data;
+    },
+  },
+};
 </script>
 
 <style scoped></style>
